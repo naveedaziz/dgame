891d78bde137254a85c27e6644a8a43c84fef0b4
Naveed Aziz <naveed.aziz@bramerz.pk>
2017-10-05 15:17:49 +0500

    DE

--- index.js
+++ index.js
@@ -14,6 +14,15 @@ app.get('/schedule', function (req, res) {
 app.get('/', function (req, res) {
    res.sendfile(path.join(__dirname, 'public/index.html'));
 });
+app.get('/login', function (req, res) {
+   res.sendfile(path.join(__dirname, 'public/login.html'));
+});
+app.get('/register', function (req, res) {
+   res.sendfile(path.join(__dirname, 'public/register.html'));
+});
+app.get('/results', function (req, res) {
+   res.sendfile(path.join(__dirname, 'public/results.html'));
+});
 app.get('/organization_commitee', function (req, res) {
    res.sendfile(path.join(__dirname, 'public/index.html'));
 });
